028908867756e3547fbf0d950a55af97bcd46dc3
Shayla Choque <89035883+ShaylaChoque@users.noreply.github.com>
2023-06-10 18:37:09 -0500

    Domain files

---
+++ Mealthy/Mealthy/Domain/Repository/IRecipeRepository.cs
@@ -0,0 +1,12 @@
+﻿using Mealthy.Mealthy.Domain.Model;
+
+namespace Mealthy.Mealthy.Domain.Repository;
+
+public interface IRecipeRepository
+{
+    Task<IEnumerable<Recipe>> ListAsync();
+    Task AddAsync(Recipe recipe);
+    Task<Recipe> FindByIdAsync(int id);
+    void Update(Recipe recipe);
+    void Remove(Recipe recipe);
+}
