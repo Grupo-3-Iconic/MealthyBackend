531f6226bdaec650799bd8ef1d4baedb49ccdf9b
Shayla Choque <89035883+ShaylaChoque@users.noreply.github.com>
2023-06-10 18:36:40 -0500

    Recipe controller

---
+++ Mealthy/Mealthy/Controllers/RecipeController.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using Mealthy.Mealthy.Domain.Model;
+using Mealthy.Mealthy.Domain.Service;
+using Mealthy.Mealthy.Resources;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mealthy.Mealthy.Controllers;
+
+[ApiController]
+[Route("api/v1/[controller]")]
+public class RecipeController : ControllerBase
+{
+    private readonly IRecipeService _recipeService;
+    private readonly IMapper _mapper;
+    public RecipeController(IRecipeService recipeService, IMapper mapper)
+    {
+        _recipeService = recipeService;
+        _mapper = mapper;
+    }
+    //GET api/v1/recipe
+    [HttpGet]
+    public async Task<IEnumerable<RecipeResource>> GetAllAsync()
+    {
+        var recipes = await _recipeService.ListAsync();
+        var resources = _mapper.Map<IEnumerable<Recipe>, IEnumerable<RecipeResource>>(recipes);
+        return resources;
+    }
+}
