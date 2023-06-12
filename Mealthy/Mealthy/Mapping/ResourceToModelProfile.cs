2f207069035981f2694391dc9c7469068fca6f15
Shayla Choque <89035883+ShaylaChoque@users.noreply.github.com>
2023-06-11 22:57:48 -0500

    Mapping Implementation

---
+++ Mealthy/Mealthy/Mapping/ResourceToModelProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using Mealthy.Mealthy.Domain.Model;
+using Mealthy.Mealthy.Resources;
+
+namespace Mealthy.Mealthy.Mapping;
+
+public class ResourceToModelProfile : Profile
+{
+    public ResourceToModelProfile()
+    {
+        CreateMap<SaveIngredienteResource, Ingredient>();
+        CreateMap<SaveStepResource, Step>();
+        CreateMap<SaveRecipeResource, Recipe>();
+    }
+}
