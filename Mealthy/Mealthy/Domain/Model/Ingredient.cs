a4ff90d7a39fc0866ae0cacac3189f23f5bea213
Shayla Choque <89035883+ShaylaChoque@users.noreply.github.com>
2023-06-21 00:17:59 -0500

    models updated

--- Mealthy/Mealthy/Domain/Model/Ingredient.cs
+++ Mealthy/Mealthy/Domain/Model/Ingredient.cs
@@ -4,6 +4,7 @@ public class Ingredient
 {
     public int Id { get; set; }
     public string Name { get; set; }
+    public string Unit { get; set; }
     public int Quantity { get; set; }
     //Relationships
     public int RecipeId { get; set; }
