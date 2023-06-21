b99826e05adfabc96603b5da8e722f0d6feaa0ee
Shayla Choque <89035883+ShaylaChoque@users.noreply.github.com>
2023-06-21 00:25:55 -0500

    General files updated

--- Mealthy/Program.cs
+++ Mealthy/Program.cs
@@ -1,13 +1,53 @@
+using Mealthy.Mealthy.Persistence.Repositories;
+using Mealthy.Mealthy.Domain.Repository;
+using Mealthy.Mealthy.Domain.Service;
+using Mealthy.Mealthy.Mapping;
+using Mealthy.Mealthy.Services;
+using Mealthy.Shared.Persistence.Contexts;
+using Mealthy.Shared.Persistence.Repositories;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+// Add Swagger http://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+//Add Database Connection
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddDbContext<AppDbContext>(
+    options => options.UseMySQL(connectionString)
+        .LogTo(Console.WriteLine, LogLevel.Information)
+        .EnableSensitiveDataLogging()
+        .EnableDetailedErrors()
+);
+
+// Add lower-case routing
+builder.Services.AddRouting(options => options.LowercaseUrls = true);
+
+// Dependency Injection Configuration
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+builder.Services.AddScoped<IRecipeRepository, RecipeRepository>();
+builder.Services.AddScoped<IRecipeService, RecipeService>();
+builder.Services.AddScoped<IIngredientRepository, IngredientRepository>();
+builder.Services.AddScoped<IIngredientService, IngredientService>();
+builder.Services.AddScoped<IStepRepository, StepRepository>();
+builder.Services.AddScoped<IStepService, StepService>();
+
+//AutoMapper Configuration
+builder.Services.AddAutoMapper(typeof(ModelToResourceProfile), typeof(ResourceToModelProfile));
+
+
 var app = builder.Build();
+
+// Validate the database schema
+using (var scope = app.Services.CreateScope())
+using (var context = scope.ServiceProvider.GetService<AppDbContext>())
+    context.Database.EnsureCreated();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
