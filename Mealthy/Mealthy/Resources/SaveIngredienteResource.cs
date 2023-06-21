07dc30e8729cbbdb332032385773fe7da0fe52a2
Shayla Choque <89035883+ShaylaChoque@users.noreply.github.com>
2023-06-21 14:23:06 -0500

    Delete SaveIngredienteResource.cs

--- Mealthy/Mealthy/Resources/SaveIngredienteResource.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Mealthy.Mealthy.Resources;
-
-public class SaveIngredienteResource
-{
-    public string Name { get; set; }
-    public string Quantity { get; set; }
-    public int RecipeId { get; set; }
-}
